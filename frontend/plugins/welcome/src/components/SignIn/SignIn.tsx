c6fdb8331b5d6ea0809cf9b379b97ecef42ef6de
moominzie <yammybbraimong@gmail.com>
2020-10-23 21:11:39 +0700

    Login

--- frontend/plugins/welcome/src/components/SignIn/SignIn.tsx
+++ frontend/plugins/welcome/src/components/SignIn/SignIn.tsx
@@ -1,42 +1,27 @@
-import React, { FC,useState,useEffect,Component } from 'react';
+import React, { FC,useState } from 'react';
+import { Link } from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { Alert } from '@material-ui/lab';
 import {
   Header,
   Page,
   pageTheme,
   Content,
  } from '@backstage/core';
-import { EntEmployee } from '../../api/models/EntEmployee'; // import interface Employee
-import { EntUser } from '../../api';
 import { DefaultApi } from '../../api/apis';
-
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      Group 18 SA-63{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
     marginLeft: theme.spacing(1),
-    //display: 'flex',
-   // flexDirection: 'column',
-    //alignItems: 'center',
-    //align: 'center',
+  },
+  head: {
+    marginLeft: theme.spacing(70),
     fontSize: '18px',
   },
   avatar: {
@@ -51,62 +36,96 @@ const useStyles = makeStyles(theme => ({
   },
   submit: {
     margin: theme.spacing(2, 0, 2),
+    marginLeft: theme.spacing(83),
   },
   textField: {
     width: 350 ,
-    marginLeft:7,
-    marginRight:-7,
+    marginLeft: theme.spacing(67),
    },
+   margin: {
+    margin: theme.spacing(2),
+ },
+ signin: {
+   margin: theme.spacing(2, 0, 2),
+   width: 350 ,
+   marginLeft: theme.spacing(67),
+ }
 
 }));
-class Login extends Component{
-   constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
-}
-}
+
 
 const SignIn: FC<{}> = () => {
+  const classes = useStyles();
 
-  const [users, setUsers] = useState<EntUser[]>([]);
-  const http = new DefaultApi();
-  function login(){
+  const [alert, setAlert] = useState(true);
+  const [status, setStatus] = useState(false);
 
+  const [username, setUsername] = useState(String);
+  const [password, setPassword] = useState(String);
+
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value as string);
+  };
+  
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value as string);
+  };
+
+  /*function EmployeeLogin() {
+  if (username === 'admin' && password === 'system') {
+    <Link to="/home" /> ;
+ } else {
+   setAlert(true);
+ }
+}*/
+
+const EmployeeLogin = async () => {
+  const employee = {
+    username: 'admin',
+    password: 'system',
+  };
+  console.log(employee);
+    setStatus(true);
+    console.log('Enter this Username and Password');
+  if (username == 'admin' && password == 'system') {
+    window.location.href = "http://localhost:3000/Home";
+    setAlert(true);
+    console.log('kluay');
+  } else {
+    setAlert(false);
   }
 
-  const classes = useStyles();
+  const timer = setTimeout(() => {
+    setStatus(false);
+  }, 3000);
+};
+
   return (
     <Page theme={pageTheme.tool}>
 
 <Header
-       title="Login" type="Repairing computer systems"> 
+       title="Signin" type="Repairing computer systems"> 
      </Header>
 
-     <Content align="center">
+     <Content>
   <div className={classes.paper}> <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar></div>
-     <div className={classes.paper}><strong>พนักงานศูนย์บริการแจ้งซ่อมคอมพิวเตอร์</strong></div>
-     <TextField className={classes.textField}
-    //          style={{ width: 500 ,marginLeft:7,marginRight:-7}}
+     <div className={classes.head}><strong>พนักงานศูนย์บริการแจ้งซ่อมคอมพิวเตอร์</strong></div>
+
+     <form noValidate autoComplete="off">
+     <div><TextField className={classes.textField}
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-               // value={personalid}
-                //onChange={handlePersonalIDChange}
-              />
- <div></div>
-      <TextField className={classes.textField}
-    //          style={{ width: 500 ,marginLeft:7,marginRight:-7}}
+                id="username"
+                label="Username"
+                type="username"
+                name="username"
+                onChange={handleUsernameChange}
+              /></div>
+      <div><TextField className={classes.textField}
                 variant="outlined"
                 margin="normal"
                 required
@@ -115,19 +134,26 @@ const SignIn: FC<{}> = () => {
                 label="Password"
                 type="password"
                 id="password"
-                autoComplete="current-password"
-               // value={personalid}
-                //onChange={handlePersonalIDChange}  
-              />
-<div> <Button
-            type="submit"
-            href="/home"
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button></div>
+                onChange={handlePasswordChange}
+              /></div></form>
+
+            <div> 
+            <Button
+              onClick={() => {EmployeeLogin();}}
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button></div>
+
+            {status ? ( 
+                      <div className={classes.signin}>
+              { alert ? ( 
+                    <Link to="/home" />   ) 
+              : ( <Alert variant="outlined" severity="info"> Incorrect Username or Password </Alert> )} </div>
+            ) : null}
      </Content>
     </Page>
   );
